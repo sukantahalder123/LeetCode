711ffaa332809df19f81aa3744bfe1f176d412f7
Sukanta Halder <128042790+sukantahalder123@users.noreply.github.com>
2025-03-18 20:40:14 +0530

    Time: 0 ms (100%), Space: 57.5 MB (29.43%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if (nums.length === 0)return 0
+    // if (nums.length === 0)return 0
     let k = 1
     for(let i =1 ; i < nums.length; i++){
         if(nums[i]!==nums[i - 1 ]){
